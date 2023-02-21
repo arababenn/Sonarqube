411b703420fec3c8302b698203de6b10c382f5c7
Benedicta <arababenn@yahoo.co.uk>
2023-02-20 20:47:09 -0600

    Installing Sonarqube

---
+++ Sonar.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Author : Benedicta
+#Date : 02/21/2023
+
+### Discription : Installing Sonarqube
+
+sudo yum update -y
+sudo yum install java-11-openjdk-devel -y
+sudo yum install java-11-openjdk -y
+cd /opt
+sudo yum install wget -y
+sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.3.0.51899.zip
+sudo unzip /opt/sonarqube-9.3.0.51899.zip
+sudo chown -R vagrant:vagrant /opt/sonarqube-9.3.0.51899
+cd /opt/sonarqube-9.3.0.51899/bin/linux-x86-64
+ ./sonar.sh start 
